c6e04a81e19b9cb2b93b474c83c4fa270847e580
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-21 18:34:50 +0100

    🚀 setup graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import "reflect-metadata";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers";
 
 (async () => {
   const app = express();
@@ -8,6 +11,15 @@ import express from "express";
   app.get("/", (_, res) => {
     res.send("hello from express !");
   });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(process.env.PORT!, () => {
     console.log(`🚀 server runing http://localhost:${process.env.PORT}`);
