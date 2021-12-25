3ccce38bd743fe7cd3b4bed3b5dd3cc9b6d47c03
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-25 17:54:58 +0100

    🥣 add routing !

--- web/src/Application.tsx
+++ web/src/Application.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { routes } from "./utils/config/routes";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  RouteComponentProps,
+} from "react-router-dom";
 
 export const Application: React.FC = () => {
   return (
     <>
-      <h1>hello from application !</h1>
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route, key) => (
+            <Route
+              key={key}
+              path={route.path}
+              exact={route.exact}
+              render={(props: RouteComponentProps) => (
+                <route.component {...route.props} {...route.name} {...props} />
+              )}
+            />
+          ))}
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
