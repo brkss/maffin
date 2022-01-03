9c167a5ae07955cfab2e079903f8c425a248c357
Brahim Berkasse <berkassebrahim@gmail.com>
2022-01-03 16:23:18 +0100

    🖍  Logout button UI !

--- web/src/components/Navigation/Top.tsx
+++ web/src/components/Navigation/Top.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Box, Container, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Text, Grid, GridItem, Button } from "@chakra-ui/react";
 
 export const TopNavigation: React.FC = () => {
+  const logout = () => {};
+
   return (
-    <Box>
-      <Container>
-        <Grid templateColumns="repeat(12, 1fr)">
-          <GridItem colSpan={6}>
-            <p>N</p>
-          </GridItem>
-          <GridItem colSpan={6}>
-            <p>N</p>
-          </GridItem>
-        </Grid>
-      </Container>
+    <Box p={"5px 10px"} bg={"gray.50"}>
+      <Grid templateColumns="repeat(12, 1fr)">
+        <GridItem colSpan={6}>
+          <Text fontWeight={"bold"}>Maffin</Text>
+        </GridItem>
+        <GridItem colSpan={6} textAlign={"right"}>
+          <Button size={"sm"}>Logout</Button>
+        </GridItem>
+      </Grid>
     </Box>
   );
 };
