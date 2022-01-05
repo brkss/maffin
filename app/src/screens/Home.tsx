0120e21a2f98bd61a1edb16d49f40af7c7eb3197
Brahim Berkasse <berkassebrahim@gmail.com>
2022-01-05 17:23:08 +0100

    👋 logout on press [PASSED]

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Button } from "native-base";
+import { AuthContext } from "../utils/auth/AuthProvider";
 
 export const Home: React.FC = () => {
+  const { logout } = React.useContext(AuthContext);
+
   return (
     <View style={styles.container}>
       <Text>This Home </Text>
+      <Button colorScheme={"dark"} onPress={async () => await logout()}>
+        LOGOUT
+      </Button>
     </View>
   );
 };
