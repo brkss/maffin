8f462d7179e4494abbecd393f463e6c8b81fd8b3
Brahim Berkasse <berkassebrahim@gmail.com>
2022-01-15 19:37:48 +0100

    🥣 checkout page !

---
+++ web/src/pages/Payement/Checkout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Center, Heading } from "@chakra-ui/react";
+
+export const Checkout: React.FC = () => {
+  return (
+    <Center h={"100vh"}>
+      <Heading>Payment</Heading>
+    </Center>
+  );
+};
